dd6cd36f11996704a2a1e0fc71d086c11c9cd4a2
Flavius <flaviusbejan96@yahoo.com>
2022-09-22 10:22:31 +0300

    Finisg

---
+++ exercise.js
@@ -0,0 +1,12 @@
+const student = {
+  id: 1,
+  school: "Liceo",
+  year: 3,
+  personalData: {
+    myName: "Flavius",
+    mySurname: "Bejan",
+    myAge: 26,
+  },
+};
+
+console.log(student.personalData);
